5de44a900ff22fdd15e58e4c4071e74efd44ed74
Daniel D. Beck <daniel@ddbeck.com>
2023-11-21 12:29:01 +0100

    Add minimalist Browser class

--- src/browser.test.ts
+++ src/browser.test.ts
@@ -6,4 +6,10 @@ describe("browser()", function () {
   it("throws for non-existent browsers", function () {
     assert.throws(() => browser("crumpet"), Error);
   });
+
+  describe("Browser", function () {
+    it("#toString()", function () {
+      assert.equal(`${browser("chrome")}`, "[Browser Chrome]");
+    });
+  });
 });
--- src/browser.ts
+++ src/browser.ts
@@ -1,6 +1,22 @@
+import { BrowserName, BrowserStatement } from "@mdn/browser-compat-data";
+
 import { query } from "./query";
 
-export function browser(name: string): void {
-  const data = query(`browsers.${name}`);
-  console.log(data);
+export function browser(name: string): Browser {
+  const data = query(`browsers.${name}`) as BrowserStatement;
+  return new Browser(name as BrowserName, data);
+}
+
+export class Browser {
+  id: BrowserName;
+  data: Partial<BrowserStatement>;
+
+  constructor(id: BrowserName, data: BrowserStatement) {
+    this.id = id;
+    this.data = data;
+  }
+
+  toString(): string {
+    return `[Browser ${this.data.name}]`;
+  }
 }
