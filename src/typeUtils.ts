38c3b11cbb527bb4a9d013e788f2f2a6327b5b41
Daniel D. Beck <daniel@ddbeck.com>
2023-12-18 17:17:27 +0100

    Fix bad import

--- src/typeUtils.ts
+++ src/typeUtils.ts
@@ -5,7 +5,7 @@ import {
   MetaBlock,
   Browsers,
 } from "@mdn/browser-compat-data";
-import { CompatStatement } from "@mdn/browser-compat-data/types/types";
+import { CompatStatement } from "@mdn/browser-compat-data";
 
 export function isIndexable(o: unknown): o is Record<string, unknown> {
   if (typeof o === "object" && o !== null && Object.keys(o).length > 0) {
