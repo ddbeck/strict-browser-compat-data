c28601a390a580021a493d4a59d15c9ea42d80eb
Daniel D. Beck <daniel@ddbeck.com>
2024-02-05 14:18:15 +0100

    Fix imports, probably

---
+++ src/browser-compat-data/index.ts
@@ -0,0 +1,7 @@
+export * as browser from "./browser";
+export * as feature from "./feature";
+export * as query from "./query";
+export * as release from "./release";
+export * as supportStatements from "./supportStatements";
+export * as typeUtils from "./typeUtils";
+export * as walk from "./walk";
