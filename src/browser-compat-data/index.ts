ad3b20035418e3a75ba75ef6eba7954f72a6b4b1
Daniel D. Beck <daniel@ddbeck.com>
2024-02-11 18:45:00 +0100

    Make reexports work less weirdly

--- src/browser-compat-data/index.ts
+++ src/browser-compat-data/index.ts
@@ -1,7 +1,11 @@
-export * as browser from "./browser";
-export * as feature from "./feature";
-export * as query from "./query";
-export * as release from "./release";
-export * as supportStatements from "./supportStatements";
-export * as typeUtils from "./typeUtils";
+// TODO: explicit reexports
+export { browser, Browser } from "./browser";
+export { feature, Feature } from "./feature";
+export { query } from "./query";
+export { Release } from "./release";
+export {
+  statement,
+  SupportStatement,
+  RealSupportStatement,
+} from "./supportStatements";
 export { walk } from "./walk";
